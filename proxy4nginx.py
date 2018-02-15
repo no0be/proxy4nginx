7aec9fa96acfb9b3412c7d49c3548f6ab2aec5a0
Samy Baiwir <no0be@pm.me>
2018-02-15 21:15:50 +0100

    Few improvements

--- proxy4nginx.py
+++ proxy4nginx.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python
 #
-# Edit the nginx configuration to quickly change
-# the proxy_pass destination on-demand.
+# Automates the creation and deletion of a simple reverse proxy vhost for nginx.
 #
-# Written by: Samy BAIWIR
-# https://twitter.com/gilgha
-#
+# Author: Samy Baiwir 
+
 import sys
 import argparse
 import os
 from subprocess import call
 
 # general parameters
-proxy4nginx_dir	= '/opt/proxy4nginx'
-nginx_dir		= '/etc/nginx'
-configfile 		= 'proxy4nginx.conf'
-templatefile 	= 'config_template'
-virtualhost		= 'proxy'
+install_path	= '/opt/proxy4nginx'
+vhost_path	= '/etc/nginx/conf.d'
+vhost_name	= 'proxy'
 
-# random manipulations on general parameters
-if not nginx_dir.endswith('/'): nginx_dir += '/'
-if not proxy4nginx_dir.endswith('/'): proxy4nginx_dir += '/'
+template	= 'config.template'
 
-# constructed general parameters :-)
-sitesenabled_path	= nginx_dir + 'sites-enabled/' + virtualhost
-configfile_path		= proxy4nginx_dir + configfile
-templatefile_path 	= proxy4nginx_dir + templatefile
+# unify paths with trailing /
+if not vhost_path.endswith('/'): vhost_path += '/'
+if not install_path.endswith('/'): install_path += '/'
 
 # main functions definition
 def proxy_enable(args):
@@ -39,48 +32,48 @@ def proxy_enable(args):
 
 	proxy_pass += args.URL
 
-	# edit the nginx configfile file with the new value based on the templatefile
-	if args.verbose: print 'Edit %s with \'%s\' as new proxy_pass destination' % (configfile, proxy_pass)
+	# edit the nginx config_file file with the new value based on the template_file
+	if args.verbose: print 'Edit %s with \'%s\' as new proxy_pass destination' % (vhost_name + ".conf", proxy_pass)
 
 	replacement = {'$_PROXYPASS_$':proxy_pass}
 
-	with open(templatefile_path) as infile, open(configfile_path, 'w') as outfile:
+	with open(install_path + template_file) as infile, open(install_path + vhost_name + ".conf", 'w') as outfile:
 	    for line in infile:
 	        for src, target in replacement.iteritems():
 	            line = line.replace(src, target)
 	        outfile.write(line)
 
-	# create the symlink in sites-enabled nginx directory
-	if not os.path.isfile(sitesenabled_path):
-		if args.verbose: print 'Create the %s symlink' % sitesenabled_path
-		os.symlink(configfile_path, sitesenabled_path)
+	# create the symlink in nginx vhost directory
+	if not os.path.isfile(vhost_path + vhost_name + ".conf"):
+		if args.verbose: print 'Create the %s symlink' % vhost_name + ".conf"
+		os.symlink(install_path + vhost_name + ".conf", vhost_path + vhost_name + ".conf")
 
 def proxy_disable(args):
-        # delete the symlink in sites-enabled nginx directory
-	if not os.path.isfile(sitesenabled_path):
+        # delete the symlink in nginx vhost directory
+	if not os.path.isfile(vhost_path + vhost_name + ".conf"):
                 print "proxy4nginx already disabled..."
 		sys.exit()
 
 	else:
-		if args.verbose: print 'Delete the %s symlink' % sitesenabled_path
-		os.remove(sitesenabled_path)
+		if args.verbose: print 'Delete the %s symlink' % vhost_path + vhost_name + ".conf"
+		os.remove(vhost_path + vhost_name + ".conf")
 
 # args management
-parser = argparse.ArgumentParser(description='Edit the nginx configuration to quickly change the proxy_pass destination.')
+parser = argparse.ArgumentParser(description='Automates the creation and deletion of a simple reverse proxy vhost for nginx.')
 parser.add_argument('-v', '--verbose', action='store_true', help='blah blah blah...')
 
 subparsers = parser.add_subparsers()
 
-parser_enable = subparsers.add_parser('enable', help='enable the proxy virtualhost')
-parser_enable.add_argument('URL', help='fully-qualified domain name (ex: gateway.lan or 192.168.1.1)')
+parser_enable = subparsers.add_parser('enable', help='create the proxy vhost')
+parser_enable.add_argument('URL', help='fully-qualified domain name (ex: intranet.int or 192.168.1.1)')
 parser_enable.add_argument('-s', '--https', action='store_true', help='use https instead of http')
 parser_enable.set_defaults(func=proxy_enable)
 
-parser_disable = subparsers.add_parser('disable', help='disable the proxy virtualhost')
+parser_disable = subparsers.add_parser('disable', help='delete the proxy virtualhost')
 parser_disable.set_defaults(func=proxy_disable)
 
 args = parser.parse_args()
 args.func(args)
 
 # reload nginx configuration
-call("/etc/init.d/nginx reload", shell=True)
+call("systemctl reload nginx", shell=True)
